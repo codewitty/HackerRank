b63a16bfcb71f966bae5c32b85af436964b27bc3
Joshua Gomes <gomesjoshua@gmail.com>
2019-03-03 15:33:36 -0800

    new fns

---
+++ PracticeAlgorithms/Library_Fine.cpp
@@ -0,0 +1,126 @@
+/*Your local library needs your help! Given the expected and actual return dates for a library book, create a program that calculates the fine (if any). The fee structure is as follows:
+
+If the book is returned on or before the expected return date, no fine will be charged (i.e.: .
+If the book is returned after the expected return day but still within the same calendar month and year as the expected return date, .
+If the book is returned after the expected return month but still within the same calendar year as the expected return date, the .
+If the book is returned after the calendar year in which it was expected, there is a fixed fine of .
+Charges are based only on the least precise measure of lateness. For example, whether a book is due January 1, 2017 or December 31, 2017, if it is returned January 1, 2018, that is a year late and the fine would be .
+
+Function Description
+
+Complete the libraryFine function in the editor below. It must return an integer representing the fine due.
+
+libraryFine has the following parameter(s):
+
+d1, m1, y1: returned date day, month and year
+d2, m2, y2: due date day, month and year
+Input Format
+
+The first line contains  space-separated integers, , denoting the respective , , and  on which the book was returned. 
+The second line contains  space-separated integers, , denoting the respective , , and  on which the book was due to be returned.
+
+Constraints
+
+Output Format
+
+Print a single integer denoting the library fine for the book received as input.
+
+Sample Input
+
+9 6 2015
+6 6 2015
+Sample Output
+
+45
+*/
+
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the libraryFine function below.
+int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
+
+    int fine = 0;
+    if ((y1 - y2) > 0) {
+        fine = 10000;
+    }
+    else if ((y2 - y1) > 0) {
+        fine = 0;
+    }
+    else if ((m1 - m2) > 0) {
+        fine = (m1 - m2) * 500;
+    } else if ((y1 - y2) == 0 && (m1 - m2) == 0 && (d1 - d2) > 0) {
+      fine = (d1 - d2) * 15;
+    }
+    return fine;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string d1M1Y1_temp;
+    getline(cin, d1M1Y1_temp);
+
+    vector<string> d1M1Y1 = split_string(d1M1Y1_temp);
+
+    int d1 = stoi(d1M1Y1[0]);
+
+    int m1 = stoi(d1M1Y1[1]);
+
+    int y1 = stoi(d1M1Y1[2]);
+
+    string d2M2Y2_temp;
+    getline(cin, d2M2Y2_temp);
+
+    vector<string> d2M2Y2 = split_string(d2M2Y2_temp);
+
+    int d2 = stoi(d2M2Y2[0]);
+
+    int m2 = stoi(d2M2Y2[1]);
+
+    int y2 = stoi(d2M2Y2[2]);
+
+    int result = libraryFine(d1, m1, y1, d2, m2, y2);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+
