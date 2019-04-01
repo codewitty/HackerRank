c3389cc6b308adc199bd7874452088cf5e3cbb47
Joshua G <gomesjoshua@gmail.com>
2019-03-31 21:19:25 -0700

    Create PlusMinus.cpp

---
+++ PracticeAlgorithms/PlusMinus.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the plusMinus function below.
+void plusMinus(vector<int> arr) {
+    float i = arr.size();
+    /*while (arr[i]) {
+        i++;
+    }
+    */
+    int countNegative = 0;
+    int countPositive = 0;
+    int countZero = 0;
+
+    for (int n = 0; n < i; n++) {
+        if (arr[n] < 0) {
+            countNegative += 1;
+        }
+    }
+    double averageNeg = countNegative / i;
+
+    for (int n = 0; n < i; n++) {
+        if (arr[n] > 0) {
+            countPositive += 1;
+        }
+    }
+    double averagePos = countPositive / i;
+
+    for (int n = 0; n < i; n++) {
+      if (arr[n] == 0) {
+        countZero += 1;
+      }
+    }
+    double averageZero = countZero / i;
+
+    cout << showpoint << setprecision(6) << (float)averagePos << endl << (float)averageNeg << endl << (float)averageZero << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    plusMinus(arr);
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
