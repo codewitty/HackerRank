92c2f357f35ff4ae74677b7d389c210517531c55
Joshua G <gomesjoshua@gmail.com>
2020-04-06 13:04:43 -0700

    Create Day 13: Abstract Classes.cpp

---
+++ 30-days-of-code/Day 13: Abstract Classes.cpp
@@ -0,0 +1,26 @@
+// Write your MyBook class here
+class MyBook: public Book {
+    public:
+    //   Class Constructor
+    //   
+    //   Parameters:
+    //   title - The book's title.
+    //   author - The book's author.
+    //   price - The book's price.
+    //
+    // Write your constructor here
+    int price;
+    MyBook(string title, string author, int prices) : Book(title, author) {
+            price = prices;
+        }
+
+    
+    //   Function Name: display
+    //   Print the title, author, and price in the specified format.
+    //
+    // Write your method here
+    void display() override {
+        cout << "Title: " << title << endl << "Author: " << author << endl << "Price: " << price;
+    }
+};
+// End class
