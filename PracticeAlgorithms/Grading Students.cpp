b599ac8010108cbc3bda9442fee80251020c5233
Joshua G <gomesjoshua@gmail.com>
2020-04-10 11:38:42 -0700

    Update Grading Students.cpp

--- PracticeAlgorithms/Grading Students.cpp
+++ PracticeAlgorithms/Grading Students.cpp
@@ -24,4 +24,40 @@ vector<int> gradingStudents(vector<int> grades) {
 
 //Driver Code for testing
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
 
+using namespace std;
+
+vector<int> gradingStudents(vector<int> grades) {
+    vector<int> res;
+    for (int i = 0; i < grades.size(); ++i) {
+        if (grades[i] < 38){
+            res.push_back(grades[i]);
+        }
+        else if (grades[i] % 5 >= 3){
+            int num = grades[i];
+            while (num % 5 != 0) {
+                num += 1;
+            }
+            res.push_back(num);
+        }
+        else {
+            res.push_back(grades[i]);
+        }
+    }
+    return res;
+
+}
+
+int main() {
+  vector <int> a = {73, 67, 33, 38, 44, 77, 46, 78};
+  cout << a.size();
+  vector <int> b = gradingStudents(a);
+  for (auto i:b){
+    cout << "Grade: " << i << endl;
+  }
+    return 0;
+}
